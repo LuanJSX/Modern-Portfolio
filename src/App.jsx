23bed8705fb5c6518fed1c243e49ab71a9b160f7
Luan Batista <69101674+LuanJSX@users.noreply.github.com>
2025-07-15 18:13:09 -0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [isloaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+         setIsLoaded(true)
+  },[])
   return (
-    <div className="app">
+    <div className= {`app ${isloaded ? "loaded" : ""}`}>
       <Navbar />
       
       <Hero/>
